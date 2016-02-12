92123781a4d38cbf9ae9ebe7e2f7789ca1d60572
Renjun Zheng <zheng174@purdue.edu>
2016-02-12 11:49:55 -0500

    Changed navigation drawer a bit. But didn't manage to set the navigation drawer to the right. I thought it might be a good idea because when user goes to next screen, there is a back arrow on the top. Though I haven't tested to see the effect. It is not hard to imagine it won't be pretty.

--- app/src/main/java/com/renjunzheng/vendingmachine/VMSelection.java
+++ app/src/main/java/com/renjunzheng/vendingmachine/VMSelection.java
@@ -9,6 +9,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -82,7 +83,9 @@ public class VMSelection extends AppCompatActivity
 
         } else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_report) {
+
+        }else if (id == R.id.nav_rate) {
 
         }
 
