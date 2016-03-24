b534025538944f233a82d4b2f773347afd9b036c
Renjun Zheng <zheng174@purdue.edu>
2016-03-24 11:17:26 -0400

    Couple Things has been done. 1. Created Splash Screen for registering the GCM service and get the token. // basically it provides what it need to registrationintentservice to register in the background. After finish, splash screen will just receive the registration token from the brodcast listener. 2. Added RegistrationIntentService, QuickStartPreferences, MyInstanceIDListenerService, MyGcmListenerService from google provided template. Haven't implemented anything yet. 3. Updated both module and application level build.gradle file, also the gradle-wrapper.properties file. It was required to do so in order to use GCM. 4. Changed layout of the navigation drawer in nav_head_main.  It looks better than my original idea. 5. Updated rate button in the VMSelection, but the code is not tested because this app is not yet published. So it will lead to nowhere in the app store.
    Things need to be done in future:
    1. Haven't implement send the registrationtoken to the server yet.
    2. Test the behavior of receiving message.
    
    Things been considered:
    1. One thing really important for creating the application splash screen, don't use image that size is really large. I tried image take 2MB, it always run out of memory. It left no memory behind for the VMSelection class

--- app/src/main/java/com/renjunzheng/vendingmachine/VMSelection.java
+++ app/src/main/java/com/renjunzheng/vendingmachine/VMSelection.java
@@ -1,6 +1,8 @@
 package com.renjunzheng.vendingmachine;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -87,8 +89,22 @@ public class VMSelection extends AppCompatActivity
 
         } else if (id == R.id.nav_report) {
 
-        }else if (id == R.id.nav_rate) {
+        } else if (id == R.id.nav_rate) {
+            //Todo: following code leads to the play store, but the intent won't be found because I never publish it before
 
+            Uri uri = Uri.parse("market://details?id=" + getApplicationContext().getPackageName());
+            Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+            // To count with Play market backstack, After pressing back button,
+            // to taken back to our application, we need to add following flags to intent.
+            goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
+                    Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET |
+                    Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+            try {
+                startActivity(goToMarket);
+            } catch (ActivityNotFoundException e) {
+                startActivity(new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("http://play.google.com/store/apps/details?id=" + getApplicationContext().getPackageName())));
+            }
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
